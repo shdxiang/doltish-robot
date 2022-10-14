0817414a624f31d6cf8244258bb9bbb79c918d7e
shengdong.xiang <shengdong.xiang@nautiluscapital.net>
2022-10-14 09:45:43 +0800

    close if already started

--- dr.py
+++ dr.py
@@ -6,6 +6,7 @@ import time
 from pywinauto.application import Application
 from pywinauto import keyboard
 
+
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
@@ -23,21 +24,38 @@ interval = config['interval']
 cmd_line = config['cmd_line']
 vk_packet = config.get('vk_packet', True)
 
-# start and connect application
+# close if already running
+try:
+    app = Application().connect(path=cmd_line, timeout=2)
+    app.kill()
+except Exception as e:
+    pass
+time.sleep(interval)
+
+# start application
 Application().start(cmd_line, work_dir=config.get('work_dir'))
 time.sleep(interval)
+
+# connect application
 app = Application().connect(path=cmd_line)
 time.sleep(interval)
 
+# set focus
+app.top_window().set_focus()
+
 # actions
 for action in config['actions']:
-    if action['type'] == 'key':
+    act_type = action['type']
+    act_value = action.get('value')
+
+    if act_type == 'key':
         keyboard.send_keys(
             action['value'], with_spaces=True, vk_packet=vk_packet)
-    elif action['type'] == 'input':
-        value = action['value']
-        value = input(f'{value}:')
-        keyboard.send_keys(value, with_spaces=True, vk_packet=vk_packet)
+    elif act_type == 'input':
+        value = input(f'{act_value}:')
         app.top_window().set_focus()
+        keyboard.send_keys(value, with_spaces=True, vk_packet=vk_packet)
+    elif act_type == 'wait':
+        time.sleep(act_value)
 
     time.sleep(interval)
